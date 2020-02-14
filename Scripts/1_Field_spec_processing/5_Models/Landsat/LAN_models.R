a3274697ef4e852a989b3ed0e129cd81a0aa636c
Kevaughan Smith <47826810+Kevaughan876@users.noreply.github.com>
2020-02-14 08:24:00 -0500

    Updated
    Updated

--- Scripts/1_Field_spec_processing/5_Models/Landsat/LAN_models.R
+++ Scripts/1_Field_spec_processing/5_Models/Landsat/LAN_models.R
@@ -4,11 +4,16 @@ library(randomForest)
 library(randomForestExplainer)
 
 ###Reads in all predictors for scans
-alaskaSpeclib_LAN_VIs<-read.csv("Outputs/1_Field_spec/1_Processing/Landsat_data/alaskaSpeclib_LAN_VIs.csv")
+Lichen_groups_LAN_VIs<-read.csv("Outputs/1_Field_spec/1_Processing/Landsat_data/Lichen_groups_LAN_VIs.csv")
 
 ##we'll need to apply a function to all dataframes that omits unwanted metadata
-alaskaSpeclib_LAN_VIs       [c("ScanID","PFT","PFT_2","PFT_4","area","Freq1","Freq2","Freq4")] = NULL
+Lichen_groups_LAN_VIs      [c("ScanID","PFT","PFT_2.x","PFT_3.x","PFT_4","veg_lifeform_code","group","Freq1","Freq2","Freq3","area" )] = NULL
 
 set.seed(2017)
 ##Lets run some different models so we can asses whiich ones are the best for prediction later
-rf_LAN_VIs   <-randomForest(PFT_3~.,data=alaskaSpeclib_LAN_VIs        ,mtry=sqrt(ncol(alaskaSpeclib_LAN_VIs        )),ntree=1001,localImp = TRUE)
+rf_LAN_VIs   <-randomForest(color~.,data=Lichen_groups_LAN_VIs       ,mtry=sqrt(ncol(Lichen_groups_LAN_VIs        )),ntree=1001,localImp = TRUE)
+
+##Lets create a data frame that will combine the class.error of all the categories of each model
+rf_LAN_VIs_confusion   <-rf_LAN_VIs   $confusion%>%as.data.frame()%>%dplyr::select(class.error)%>%rename(rf_LAN_VIs =class.error )
+
+write.csv(rf_LAN_VIs_confusion,"Outputs/1_Field_spec/2_Models/Landsat/rf_LAN_VIs_confusion.csv")
