83a86234539a3a26737b1d589e1c2617f4c405a7
nelsopet <peter.ross.nelson@gmail.com>
2021-10-12 08:50:23 -0400

    classification fails for tiny testing quadrat dataset

--- Scripts/5_Classify_Image1.R
+++ Scripts/5_Classify_Image1.R
@@ -34,7 +34,8 @@ source("Functions/LandCoverEstimator.R")
 #list.files("F:/TwelveMile")
 
 system.time(PredLayer <- LandCoverEstimator(
-    filename = "Data/TwelveMile_2019_08_09_21_28_52_0_rd_rf_or",
+  filename = "Data/Ground_Validation/BisonGulchQuads",  
+  #filename = "Data/TwelveMile_2019_08_09_21_28_52_0_rd_rf_or",
     #    filename = "F:/TwelveMile/TwelveMile_2019_08_09_21_10_22_2000_rd_rf_or",
     #filename = "Data/TwelveMile_2019_08_09_21_28_52_0_rd_rf_or",
 #    filename = "M:/Alaska_Datacubes/Raw_files/WickershamDome_2019_08_08_19_31_51_2000_rd_rf_or",
