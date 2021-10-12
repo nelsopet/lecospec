e9649003f5d7f7e8d755184035b7ae6573833049
unknown <peter.ross.nelson@gmail.com>
2021-10-12 11:51:43 -0400

    changed image chopper fnc to read in brick rather than raster. this required changes to how it cropped. It not crops the full image by the extent of the quadrats a nd then masks out everything not in the quadrat otherwise R hangs up.

--- Functions/Image_Chopper.R
+++ Functions/Image_Chopper.R
@@ -1,10 +1,10 @@
 
 ImgChopper<-
 function(img,quad) {
-tst_img<-raster(img)
+tst_img<-brick(img)
 tst_quads<-readOGR(dsn=quad)
 tst_crop<-raster::crop(tst_img, tst_quads)
-tst_mask<-raster::mask(tst_img, tst_quads)
+tst_mask<-raster::mask(tst_crop, tst_quads)
 tst_out<-c(tst_crop,tst_mask)
 return(tst_out)
 }
