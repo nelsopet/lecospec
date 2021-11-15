1d68d5bdd174c4411199b6c20141563bf8147923
unknown <peter.ross.nelson@gmail.com>
2021-11-15 15:47:34 -0500

    testing models on another validation dataset

--- Scripts/Crop_Training_Quads.R
+++ Scripts/Crop_Training_Quads.R
@@ -13,13 +13,10 @@ Bison_out<-ImgChopper(Bison_path, Bison_seg_path)
 #plot(Bison_out[[2]])
 writeRaster(Bison_out[[2]], "./Data/Ground_Validation/BisonGulchQuads", format = "ENVI", overwrite = TRUE)
 
-
-
-EightMile_path = "F:/EightMile/raw_2374_rd_or"
+EightMile_path = "F:/EightMile/raw_2374_rd_rf_or"
 EighMile_seg_path = "F:/EightMile/EightMile_Quadrats_ALL.shp"
 EightMile_out<-ImgChopper(EightMile_path, EighMile_seg_path)
-#plot(EightMile_out[[2]])
-writeRaster(EightMile_out[[2]], "./Data/Ground_Validation/EightMileQuads", format = "ENVI", overwrite = TRUE)
+plot(EightMile_out$X397.593.nm)
 
 TwelveMile_path = "F:/TwelveMile/TwelveMile_2019_08_09_21_28_52_0_rd_rf_or"
 TwelveMile_seg_path = "F:/TwelveMile/TwelveMileQ0_10_20_30_40m.shp"
@@ -35,6 +32,7 @@ TwelveMile_out2<-ImgChopper(TwelveMile_path2, TwelveMile_seg_path2)
 #TwelveMile_out<-do.call(merge, TwelveMile_list)
 
 #plot(TwelveMile_out[[2]])
+writeRaster(EightMile_out, "./Data/Ground_Validation/EightMileQuads", format = "ENVI", overwrite = TRUE)
 writeRaster(TwelveMile_out1, "./Data/Ground_Validation/TwelveMileGulchQuads1", format = "ENVI", overwrite = TRUE)
 writeRaster(TwelveMile_out2, "./Data/Ground_Validation/TwelveMileGulchQuads2", format = "ENVI", overwrite = TRUE)
 
