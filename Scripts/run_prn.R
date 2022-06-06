e76d13b227f14f88d6684b627cd00b14159d555c
nelsopet <peter.ross.nelson@gmail.com>
2022-06-06 11:52:11 -0400

    added all vectors for quadrats for Bison, Twelve and Eight Miles, Chatanika and Murphy Dome. Extractd pixels from full images from these quad shapefiles

--- Scripts/run_prn.R
+++ Scripts/run_prn.R
@@ -4,30 +4,26 @@ test_path <- "./Data/Ground_Validation/Imagery/BisonGulchQuads.envi"
 
 raster::raster(test_path) %>% plot()
 
-test_path_2 <- "F:/Lecospec/tiles/tile_W7u8XiTLq7S1hdvM.grd"
-model_path <- "C:/Users/kenne/Documents/GitHub/lecospec/mle/RandomForest_FncGrp1_1000trees_Augmented.rda"
+test_path_2 <- "tiles/tile_CpDHDDdNOXTuX0G4.tif"
+model_path <- "C:/Users/kenne/Documents/GitHub/lecospec/Output/E_003_Pred_Model_RandomForest_FncGrp1_1000trees.rda"
 
-big_test <- "E:/ORNL_DAAC_DATA_ARCHIVE/MurphyDome/MurphyDome_2018_07_31_19_47_11_10350_rd_rf_or"
+#big_test <- "E:/ORNL_DAAC_DATA_ARCHIVE/WickershamDome/WickershamDome_2019_08_08_19_31_51_0_rd_rf_or"
+#big_test <- "E:/ORNL_DAAC_DATA_ARCHIVE/MurphyDome/MurphyDome_2018_07_31_19_47_11_10350_rd_rf_or"
+#big_test <- "E:/ORNL_DAAC_DATA_ARCHIVE/Chatnika/Chatnika_2018_07_29_20_32_59_0_rd_rf_or"
+#big_test<-"E:/ORNL_DAAC_DATA_ARCHIVE/LittleLake/LittleLake_2018_07_31_01_09_59_132_rd_rf_or"
+#big_test<-"E:/ORNL_DAAC_DATA_ARCHIVE/BigTrailLake/BigTrailLake_2019_08_07_21_33_06_0_rd_rf_or"
+#big_test<-"E:/ORNL_DAAC_DATA_ARCHIVE/BigTrailLake/BigTrailLake_2019_08_08_00_25_53_2000_rd_rf_or"
+#big_test<-"E:/ORNL_DAAC_DATA_ARCHIVE/ClaytonLake/ClaytonLake_2018_07_27_01_22_25_4000_rd_rf_or"
+big_test<-"E:/ORNL_DAAC_DATA_ARCHIVE/EagleSummit/EagleSummit_2019_08_06_00_57_02_2546_rd_rf_or"
 medium_test <- "./Data/SubsetDatacube"
 
-# 
 print(date())
 quad_results <- estimate_land_cover(
-    test_path, 
-    output_filepath = "./Output/bg_fncgrp1_outputs_m.grd",
-    use_external_bands = TRUE)
+  test_path, 
+  output_filepath = "./Output/bg_fncgrp1_outputs_m.grd",
+  use_external_bands = TRUE)
 closeAllConnections()
 print(date())
-
-
-loaded_quad_results <- raster::raster("./Output/bg_outputs_m.grd")
-
-print(loaded_quad_results)
-
-print(quad_results)
-
-windows();hist(quad_results)
-windows();hist(loaded_quad_results)
 
 print(quad_results)
 png("./test_results.png")
@@ -39,14 +35,14 @@ dev.off()
 raster::dataType(quad_results)
 
 medium_results  <- estimate_land_cover(
-    medium_test,
-    output_filepath = "./Outputs/subset_predictions.grd",
-    use_external_bands = TRUE)
+  medium_test,
+  output_filepath = "./Outputs/subset_predictions.grd",
+  use_external_bands = TRUE)
 print(date())
 
 big_results <- estimate_land_cover(
-    big_test,
-    output_filepath = "./Output/md_10350_fncgrp1_PREDICTIONS.grd")
+  big_test,
+  output_filepath = "./Output/es_02_2546_fncgrp1_PREDICTIONS.grd")
 print(date())
 
 output <- raster::raster("./Output/bison_gulch_outputs_par.grd")
@@ -58,27 +54,22 @@ ml_model <- load_model(model_path)
 print(ml_model$forest$independent.variable.names)
 
 # load bands
-band_names <- read.csv("./bands.csv")$x %>% as.vector()
-print(band_names)
-
-
-test_raster <- raster::brick(test_path_2)
-names(test_raster)
+bandnames <- read.csv("./bands.csv")$x %>% as.vector()
 
 cl <- raster::beginCluster()#this is actually quite slow, believe it or not
 
 print(date())
 profvis::profvis(
-    tile_results <- process_tile(
-        test_path_2, 
-        ml_model, 
-        1,
-        cluster = NULL, 
-        return_raster = TRUE, 
-        band_names = band_names,
-        save_path = "./test_raster_save.grd", 
-        suppress_output = FALSE),
-    interval = 0.01
+  tile_results <- process_tile(
+    test_path, 
+    ml_model, 
+    1,
+    cluster = NULL, 
+    return_raster = TRUE, 
+    names = bandnames,
+    save_path = "./test_raster_save.grd", 
+    suppress_output = FALSE),
+  interval = 0.01
 )
 print(date())
 
@@ -100,10 +91,10 @@ print(uf_test)
 
 
 tiles <- c(
-    "./tiles//prediction_V2tsWuMaJEzoJ2MF.grd",
-    "./tiles//prediction_xhzHX4KVl89FsfOU.grd",
-    "./tiles//prediction_kGVDd3pM4lcWqNRs.grd",
-    "./tiles//prediction_LWlMl4y7KcDL8Hw4.grd"
+  "./tiles//prediction_V2tsWuMaJEzoJ2MF.grd",
+  "./tiles//prediction_xhzHX4KVl89FsfOU.grd",
+  "./tiles//prediction_kGVDd3pM4lcWqNRs.grd",
+  "./tiles//prediction_LWlMl4y7KcDL8Hw4.grd"
 )
 
 output <- merge_tiles(tiles, "./merge_test.tif")
@@ -127,9 +118,9 @@ print(date())
 tif_test <- "./Data/BisonGulchQuads.tif"
 
 raster::writeRaster(
-    raster::brick(test_path),
-    tif_test,
-    "GTiff"
+  raster::brick(test_path),
+  tif_test,
+  "GTiff"
 )
 
 raster::writeFormats()
