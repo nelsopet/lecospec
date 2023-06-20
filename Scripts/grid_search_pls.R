4b3801e6342c36a528b742ddedab5223fe3d66fc
krbundy <kenneth.bundy@maine.edu>
2023-06-19 23:39:15 -0400

    update grid search scripts, separate

--- Scripts/grid_search_pls.R
+++ Scripts/grid_search_pls.R
@@ -1,3 +1,6 @@
+#######################################################
+#       Load Lecospec Core Functions
+#######################################################
 if(!dir.exists("Functions/")){
     setwd("../")
     if(!dir.exists("Functions")){
@@ -5,97 +8,21 @@ if(!dir.exists("Functions/")){
     }
 }
 source("Functions/lecospectR.R", echo = FALSE)
-
-base_paths <- c(
-    "corrected_and_img.csv",
-    "img_raw_raw.csv",
-    "grd_raw_raw.csv"
-    #"bison_gulch_stratified.csv",
-    #"grd_raw_corrected.csv",
-    #"img_indices_only.csv",# include veg indices
-    #"grd_indices_only.csv"
-)
+source("Scripts/search_control.R", echo = FALSE)
 
 
-outlier_functions <- list(
-    #clip = load_model("./mle/clip_transform.rda"),
-    no_treatment = function(x, ignore_cols = NULL){return(x)}# no transform
-)
-
-outlier_treatments <- c(
-    "no_treatment"#,
-    #"clip"
-)
-
-preprocess_functions <- list(
-    no_treatment = function(x, ignore_cols = NULL){return(x)},# no transform
-    min_max = columnwise_min_max_scale,
-    robust = columnwise_robust_scale,
-    standard = standardize_df
-)
-
-weight_functions <- list(
-    posterior = get_posterior_weights_from_targets,
-    balanced = targets_to_weights,
-    no_treatment = function(x){return(NULL)}# No weights
-)
-weight_treatments <- c(
-    #"no_treatment"#,
-    "balanced"#,
-    #"posterior"
-)
-
-preprocessing_treatments <- c(
-    "no_treatment"#,
-    #"min_max",
-    #"standard",
-    #"robust"
-)
-
-num_components <- c(
-    1,
-    2,
-    #3,
-    4,
-    #5,
-    #6,
-    #7#
-    8,
-    #10,
-    12,#14,
-    16,#18,
-    20,
-    24,
-    32,
-    50,
-    64#100,
-    #128,
-    #256,
-    #512#,1024
-)
-
-depth_control <- c(
-    #10,20, 
-    
-    40
-)
-
-
-seeds <- list(
-    61718,
-    11819,
-    39790,
-    736,
-    37603
-)
-
+#######################################################
+#       Load Data
+#######################################################
 test_data <- subset(read.csv("Data/gs/x_test/img_raw_raw.csv"), select = -c(X))
 test_labels <- read.csv("Data/gs/y_test/img_raw_raw.csv")$x %>% as.factor()
 #train_labels <- read.csv("Data/gs/y_train/img_raw_raw.csv")$x %>% as.factor()
 manifest_path <- "./gs_manifest_reduced_val_4sites.csv"
 
 
-
+#######################################################
+#       Training Loop: RF
+#######################################################
 for(filepath in base_paths){
     train_data <- subset(read.csv(paste0("Data/gs/x_train/", filepath)), select = -c(X))
     labels <- read.csv(paste0("Data/gs/y_train/", filepath))$x %>% as.factor()
@@ -106,24 +33,28 @@ for(filepath in base_paths){
                 for(n in num_components){
                     for(seed in seeds){
 
+
+
+                print(p_treatment)
+                print("Number of columns in train and test:")
+                print(ncol(train_data))
+                print(ncol(test_data))
+                print("Number of rows in train and number of labels:")
+                print(nrow(train_data))
+                print(length(labels))
+
                 model_id <- uuid::UUIDgenerate()
                 save_path <- paste0("mle/experiments/gs/", model_id, "/")
                 if(!dir.exists(save_path)){
                     dir.create(save_path)
                 }
 
-                if("Forb" %in% levels(labels)){
-                    levels(test_labels) <- c(levels(test_labels), c("Forb"))
-                }
-
-                rf_model_results <- train_model(
-                    #filter_df_bands(train_data), 
-                    train_data,
+                rf_model_results <- train_pls_lda(
+                    train_data, 
                     labels, 
                     test_data,
                     test_labels,
-                    ntree = n,
-                    max_depth = NULL,#d,
+                    n = n,
                     outlier_fn = outlier_functions[[o_treatment]],
                     preprocess_fn = preprocess_functions[[p_treatment]],
                     weight_fn = weight_functions[[w_treatment]],
@@ -131,29 +62,29 @@ for(filepath in base_paths){
                     seed=seed,
                     log_string = paste(n, filepath, o_treatment, p_treatment, w_treatment)
                 )
-                #print(rf_model_results)
+                print(rf_model_results)
 
                 rf_model <- rf_model_results$model
                 acc <- as.list(rf_model_results$confusion$overall)$Accuracy
-                #print(acc)
+                print(acc)
 
                 if(acc > 0.6){
-
 
                 results <- validate_model(
                     rf_model, 
                     save_path, 
                     outlier_processing = outlier_functions[[o_treatment]],
                     transform_type = preprocess_functions[[p_treatment]],
-                    pft_aggregation = 0
+                    pft_aggregation=0
                 )
 
                 aggregated_results <- aggregate_results(save_path)
 
                 # calculate validation statistics
-                #chi2 <- calculate_chi_squared_probability(aggregated_results)
+                chi2 <- calculate_chi_squared_probability(aggregated_results)
                 r2 <- calculate_validation_r2(aggregated_results)
                 rpd <- calculate_rpd(aggregated_results)
+
 
                 add_model_to_manifest(
                     model_id = model_id,
@@ -165,9 +96,8 @@ for(filepath in base_paths){
                     oob_error = rf_model$prediction.error,
                     accuracy = acc,
                     r2 = r2,
-                    chi2prob = rpd,
-                    seed = seed,
-                    logpath=manifest_path
+                    chi2prob = chi2,
+                    logpath="./gs_manifest_pls_ncomp.csv"
                 )
 
                 plot_by_pft(
@@ -195,12 +125,10 @@ for(filepath in base_paths){
                         accuracy = acc,
                         r2 = "Skipped",
                         chi2prob = "Skipped",
-                        seed = seed,
                         logpath=manifest_path
                     )
-
                 }
-                    }
+                }
                 }
             }
         }
