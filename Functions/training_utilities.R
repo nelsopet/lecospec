101facaeb538adaffccdb2c0406e24506ddcf1b7
krbundy <kenneth.bundy@maine.edu>
2023-07-27 11:29:17 -0400

    saved files, feature selection

--- Functions/training_utilities.R
+++ Functions/training_utilities.R
@@ -598,3 +598,125 @@ extract_features <- function(data){
 
 }
 
+
+
+create_patch_balanced_sample <- function(
+    data, 
+    patch_col = "UID", 
+    class_col = "FncGrp1", 
+    test_count = 15L, 
+    train_count = 300L,# rename this to reflect function
+    seed = NULL,
+    verbose = FALSE){
+
+        if(!is.null(seed)){
+            set.seed(seed)
+        }
+        train_samples <- NULL
+        test_samples <- NULL
+
+        split_data <- list()
+        patches_by_pft <- list()
+        samples_per_patch <- list()
+        samples_per_pft <- list()
+
+        split_levels <- unique(as.character(data[,patch_col]))
+        class_levels <- unique(as.character(data[,class_col]))
+
+        
+        for(li in class_levels){
+            patches_by_pft[[li]] <- list()
+            samples_per_pft[[li]] <- 0
+            
+        }
+
+        for(p in split_levels){
+            samples_per_patch[[p]] <- 0
+        }
+
+        if(verbose){
+            print("Building data for the following classes:")
+            print(class_levels)
+            print("Splitting evenly across the following patches")
+            print(split_levels)
+        }
+
+        for( i in seq_along(split_levels)){
+            # extract data for the given level
+            patch_id <- split_levels[[i]]
+            filtered_data <- data[data[,patch_col] == patch_id, ]
+            # get the PFT for the patch
+            patch_pft <- filtered_data[1,class_col]
+            # store the data in the data structures for the processing
+            split_data[[patch_id]] <- filtered_data
+            patches_by_pft[[patch_pft]] <- append(
+                patches_by_pft[[patch_pft]],
+                patch_id
+                )
+        }
+
+        # calculate the number of samples from each patch
+        for(j in seq_along(class_levels)){
+            # some
+            pft <- class_levels[[j]]
+            for(k in 1:test_count){
+                for(m in seq_along(patches_by_pft[[pft]])){
+                    if(samples_per_pft[[pft]] < test_count){
+                        current_patch_id <- patches_by_pft[[pft]][[m]]
+                        samples_per_patch[[current_patch_id]] <- samples_per_patch[[current_patch_id]] + 1
+                        samples_per_pft[[pft]] <- samples_per_pft[[pft]] + 1
+                    }
+                }
+            }
+
+        }
+
+        if(verbose){
+            print("Samples per patch")
+            print(samples_per_patch)
+            print("Samples per PFT")
+            print(samples_per_pft)
+        }
+        
+
+        # get the samples from the patches
+        for(st in split_levels){
+            
+            df <- split_data[[st]]
+            n_samp <- samples_per_patch[[st]]
+            permutation <- permute::shuffle(nrow(df))
+            if(n_samp > 0){
+
+                test_indices <- permutation[1:n_samp]
+
+                if(is.null(test_samples)){
+                    test_samples <- df[test_indices, ]
+                } else {
+                    test_samples <- rbind(test_samples, df[test_indices,])
+                    if(verbose){
+                        print(table(test_samples$FncGrp1))
+                    }
+
+                }
+            }
+
+            num_train <- min(train_count, nrow(df) - test_count)
+            train_indices <- permutation[(n_samp+1):(num_train + test_count)] %>% as.numeric()
+            if(is.null(train_samples)){
+                train_samples <- df[train_indices,]
+            } else {
+                train_samples <- rbind(train_samples, df[train_indices,])
+            }
+        }
+
+        if(sum(unlist(samples_per_patch)) != nrow(test_samples)){
+            warning("the number of rows in the dataframe are not as expected")
+        }
+
+    return(list(
+        test = test_samples,
+        train = train_samples,
+        samples_per_patch = samples_per_patch,
+        samples_per_pft = samples_per_pft)
+    )
+}
