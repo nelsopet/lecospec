87278da5f92ba069f9eb756112048d9ac2b98831
krbundy <kenneth.bundy@maine.edu>
2023-07-27 20:26:43 -0400

    udpate ground data split

--- Scripts/build_balanced_ground_data.R
+++ Scripts/build_balanced_ground_data.R
@@ -1,5 +1,7 @@
 source("Functions/lecospectR.R")
 
+
+set.seed(61718)
 base_path <- "Data/D_002_SpecLib_Derivs.csv"
 full_data <- read.csv(base_path)
 colnames(full_data)
@@ -48,5 +50,91 @@ grd_train <- subset(
 
 labels <- full_data$Functional_group1
 
-train_data <- 
+split_data <- create_patch_balanced_sample(
+    full_data,
+    patch_col = "Species_name",
+    class_col = "Functional_group1"
+)
 
+write.csv(
+    subset(split_data$train,
+    select=-c(
+        ScanID,
+        Area,
+        Code_name,
+        Species_name,
+        Functional_group1,
+        Functional_group2,
+        Species_name_Freq,
+        Functional_group1_Freq,
+        Functional_group2_Freq,
+        Genus,
+        Version,
+        File.Name,
+        Instrument,
+        Detectors,
+        Measurement,
+        Date,
+        Time,
+        Battery.Voltage,
+        Averages,
+        Integration1,
+        Integration2,
+        Integration3,
+        Dark.Mode,
+        Foreoptic,
+        Radiometric.Calibration,
+        Units,
+        Latitude,
+        Longitude,
+        Altitude,
+        GPS.Time,
+        Satellites,
+        Calibrated.Reference.Correction.File,
+        Channels,
+        ScanNum
+    )), 
+    file = "Data/v2/grd_train.csv"
+    )
+
+
+write.csv(
+    subset(
+        split_data$test,
+        select=-c(
+        ScanID,
+        Area,
+        Code_name,
+        Species_name,
+        Functional_group1,
+        Functional_group2,
+        Species_name_Freq,
+        Functional_group1_Freq,
+        Functional_group2_Freq,
+        Genus,
+        Version,
+        File.Name,
+        Instrument,
+        Detectors,
+        Measurement,
+        Date,
+        Time,
+        Battery.Voltage,
+        Averages,
+        Integration1,
+        Integration2,
+        Integration3,
+        Dark.Mode,
+        Foreoptic,
+        Radiometric.Calibration,
+        Units,
+        Latitude,
+        Longitude,
+        Altitude,
+        GPS.Time,
+        Satellites,
+        Calibrated.Reference.Correction.File,
+        Channels,
+        ScanNum
+    )), 
+    file = "Data/v2/grd_test.csv")
