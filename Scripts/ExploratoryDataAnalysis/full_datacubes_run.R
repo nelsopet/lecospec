f8b6f5c5785d456e72f194f91dca4a082ea1a397
nelsopet <peter.ross.nelson@gmail.com>
2023-10-18 11:33:06 -0400

    updated run_prn to be a normal script name now full_datacubes_run. added lists of images per flight that can be run with models for spatial statistics

--- Scripts/ExploratoryDataAnalysis/full_datacubes_run.R
+++ Scripts/ExploratoryDataAnalysis/full_datacubes_run.R
@@ -41,6 +41,22 @@ colnames(tree2)<-"splitVarIDs"
   head(tree3)
   tree3 %>% group_by(splitVarIDs,tree) %>% tally() %>% print(n=142)
 
+#Flight lines for use in manuscript
+Bison_dir="M:/Alaska_DATA/Alaska_Summer2019/Data_by_site/Bison_Gulch/Imagery_60m/100251_Bison_Gulch_line2_2019_08_12_01_07_28/"
+Bonanza_dir="M:/Alaska_DATA/Alaska_Summer2018/Workspaces/Alaska/DatabyDate/72518/ImagingSpectrometer/DataFiles/100066_2018_07_25_21_18_45/"
+EightMile_dir = "M:/Alaska_DATA/Alaska_Summer2018/Workspaces/Alaska/DatabyDate/72818/ImagingSpectrometer/DataFolders/100124_BlacktandardFlight2_2018_07_28_22_56_17/"
+Chatanika_dir = "M:/Alaska_DATA/Alaska_Summer2018/Workspaces/Alaska/DatabyDate/72918/ImagingSpectrometer/DataFolders/100130_ChatanikaFlight3_attempt2_2018_07_29_20_32_59/"
+
+#Get image names for running predictions for all datacubes in a flight
+Bison_imgs<-list.files(Bison_dir)[grepl("*_rd_rf_or$",list.files(Bison_dir))]
+Bonanza_imgs<-list.files(Bonanza_dir)[grepl("*_rd_rf_or$",list.files(Bonanza_dir))]
+EightMile_imgs<-list.files(EightMile_dir)[grepl("*_rd_rf_or$",list.files(EightMile_dir))]
+Chatanika_imgs<-list.files(Chatanika_dir)[grepl("*_rd_rf_or$",list.files(Chatanika_dir))]
+
+#Test to make sure a datacube reads in with the list of files and the path name to each site
+#brick(paste(Bison_dir,Bison_imgs[2], sep="")) #PASS
+
+#Single images selected for use for manuscript
 Bison_path ="M:/Alaska_DATA/Alaska_Summer2019/Data_by_site/Bison_Gulch/Imagery_60m/100251_Bison_Gulch_line2_2019_08_12_01_07_28/raw_1511_rd_rf_55pctWhiteRef_or"
 Bonanza_path ="M:/Alaska_DATA/Alaska_Summer2018/Workspaces/Alaska/DatabyDate/72518/ImagingSpectrometer/DataFiles/100066_2018_07_25_21_18_45/raw_6425_rd_rf_55pctWhiteRef_or"
 EightMile_path = "M:/Alaska_DATA/Alaska_Summer2018/Workspaces/Alaska/DatabyDate/72818/ImagingSpectrometer/DataFolders/100124_BlacktandardFlight2_2018_07_28_22_56_17/raw_5968_rd_rf_55pctWhiteRef_or"
@@ -48,6 +64,7 @@ Chatanika_path = "M:/Alaska_DATA/Alaska_Summer2018/Workspaces/Alaska/DatabyDate/
 TwelveMile_path = "M:/Alaska_DATA/Alaska_Summer2019/Data_by_site/12mile/Imagery/100241_12mile_line3_2019_08_09_21_28_52/raw_0_rd_rf_55pctWhiteRef_or"
 TwelveMile_path2 ="M:/Alaska_DATA/Alaska_Summer2019/Data_by_site/12mile/Imagery/100241_12mile_line3_2019_08_09_21_28_52/raw_2000_rd_rf_55pctWhiteRef_or"
 
+#Paths to original ORNL_DAAC_DATA_ARCHIVE prior to radiometric reprocessing
 #big_test <- "F:/ORNL_DAAC_DATA_ARCHIVE/BisonGulch/BisonGulch_2019_08_12_01_07_28_1511_rd_rf_or" 
 #big_test <-"F:/ORNL_DAAC_DATA_ARCHIVE/Bonanza/raw_6425_rd_rf_or"
 #big_test <- "F:/ORNL_DAAC_DATA_ARCHIVE/WickershamDome/WickershamDome_2019_08_08_19_31_51_0_rd_rf_or"
