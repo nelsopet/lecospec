d628ece60b74891b72be81ad6a831efd21d1ff54
nelsopet <peter.ross.nelson@gmail.com>
2023-10-18 12:43:19 -0400

    compared white references of tarp to spectralon by site

--- Scripts/TrainingDataCreation/Image/105_Compare_white_references_across_sites.R
+++ Scripts/TrainingDataCreation/Image/105_Compare_white_references_across_sites.R
@@ -36,10 +36,51 @@ df$Wavelength<-band_names
 
 WhiteRef_df<-Reduce(function (x,y) merge(x, y, all=TRUE), WhiteRef_df)
 colnames(WhiteRef_df)
+unique(WhiteRef_df$Site_WhiteRef_Image)
+head(WhiteRef_df)
+WhiteRef_df %>% dplyr::filter(Site_WhiteRef_Image=="Bonanza_100066_2018_07_25_21_18_45_sceneWhiteReference") %>% head
+WhiteRef_df %>% dplyr::filter(Site_WhiteRef_Image=="BisonGulch_100251_Bison_Gulch_line2_2019_08_12_01_07_28_sceneWhiteReference") %>% head
+
+#Read in new white references taken from brightest section of calibration tarp (66%)
+#Flight lines for use in manuscript
+Bison_dir="M:/Alaska_DATA/Alaska_Summer2019/Data_by_site/Bison_Gulch/Imagery_60m/100251_Bison_Gulch_line2_2019_08_12_01_07_28/"
+Bonanza_dir="M:/Alaska_DATA/Alaska_Summer2018/Workspaces/Alaska/DatabyDate/72518/ImagingSpectrometer/DataFiles/100066_2018_07_25_21_18_45/"
+EightMile_dir = "M:/Alaska_DATA/Alaska_Summer2018/Workspaces/Alaska/DatabyDate/72818/ImagingSpectrometer/DataFolders/100124_BlacktandardFlight2_2018_07_28_22_56_17/"
+Chatanika_dir = "M:/Alaska_DATA/Alaska_Summer2018/Workspaces/Alaska/DatabyDate/72918/ImagingSpectrometer/DataFolders/100130_ChatanikaFlight3_attempt2_2018_07_29_20_32_59/"
+
+#Get image names for running predictions for all datacubes in a flight
+Bison_SinglePixWhiteRef<-list.files(Bison_dir)[grepl("*sceneWhiteReference$",list.files(Bison_dir))]
+Bonanza_SinglePixWhiteRef<-list.files(Bonanza_dir)[grepl("*sceneWhiteReference$",list.files(Bonanza_dir))]
+EightMile_SinglePixWhiteRef<-list.files(EightMile_dir)[grepl("*sceneWhiteReference$",list.files(EightMile_dir))]
+Chatanika_SinglePixWhiteRef<-list.files(Chatanika_dir)[grepl("*sceneWhiteReference$",list.files(Chatanika_dir))]
+
+NewWhiteRefs<-c(paste(Bison_dir,Bison_SinglePixWhiteRef, sep=""),
+paste(Bonanza_dir,Bonanza_SinglePixWhiteRef, sep=""),
+paste(EightMile_dir,EightMile_SinglePixWhiteRef, sep=""),
+paste(Chatanika_dir,Chatanika_SinglePixWhiteRef, sep="")
+)
 
 
-jpeg("figures/WhiteRef_by_Site.jpg", height = 3000, width = 5000, res = 350)
+NewWhiteRef_df<-lapply(1:length(NewWhiteRefs), function(x){ 
+band_path<-brick(NewWhiteRefs[x])
+band_count<-names(band_path) %>% length()
+tst<-band_path
+df <- raster::rasterToPoints(tst) %>% 
+  as.data.frame() %>%
+  dplyr::select(-x,-y) %>%
+  t() %>%
+  as.data.frame()
+colnames(df)<-"Radiance"
+df$Site_WhiteRef_Image<-imgs_names[x]
+df$Wavelength<-band_names
+  return(df)
+})
 
+NewWhiteRef_df<-Reduce(function (x,y) merge(x, y, all=TRUE), NewWhiteRef_df)
+
+
+#jpeg("figures/WhiteRef_by_Site.jpg", height = 3000, width = 5000, res = 350)
+X11()
 ggplot(WhiteRef_df, aes(Wavelength, Radiance, color = Site_WhiteRef_Image), scales = "fixed")+
   theme(panel.background = element_rect(fill = "white", colour = "grey50"), 
         #legend.key.size = unit(0.5, "cm"),legend.text = element_text(size=25),
@@ -54,10 +95,72 @@ ggplot(WhiteRef_df, aes(Wavelength, Radiance, color = Site_WhiteRef_Image), scal
   y = "Radiance (mW/(cm2*sr*um))")
  dev.off()
 
+jpeg("figures/Tarp66pctReflWhiteRef_by_Site.jpg", height = 3000, width = 5000, res = 350)
+#X11()
+ggplot(NewWhiteRef_df, aes(Wavelength, Radiance, color = Site_WhiteRef_Image), scales = "fixed")+
+  theme(panel.background = element_rect(fill = "white", colour = "grey50"), 
+        #legend.key.size = unit(0.5, "cm"),legend.text = element_text(size=25),
+        #legend.position = "none",
+        title = element_text(size=25),
+        strip.text = element_text(size = 25),
+        axis.text = element_text(size = 20),
+        axis.text.x = element_text(angle = 90)) +
+  geom_line(aes(Wavelength, Radiance), size = 2)+
+  labs(title = "Bright (66% reflectance) tarp pixel radiance prior to flight by site with image name",
+  x = "Wavelength (nm)",
+  y = "Radiance (mW/(cm2*sr*um))")
+dev.off()
+
+#Compare Spectralon prior to fligh vs 66 pct reflectance tarp pixel
+WhiteRef_df$Source<-"Spectralon"
+NewWhiteRef_df$Source<-"Tarp"
+
+FullWhiteRef_df<-bind_rows(WhiteRef_df,NewWhiteRef_df) #%>% str
+colnames(FullWhiteRef_df)
+
+FullWhiteRef_df_wide<- FullWhiteRef_df  %>% 
+tidyr::pivot_wider(names_from = Source, values_from = Radiance)  %>% 
+dplyr::mutate(Wavelength = as.numeric(Wavelength), 
+Spectralon = as.numeric(Spectralon), 
+Tarp = as.numeric(Tarp), 
+Radiance_proportion = Tarp/Spectralon,
+Radiance_diff = Spectralon-Tarp) 
+
+
+jpeg("figures/Spectralon_vs_Tarp66pctReflWhiteRef_by_Site.jpg", height = 3000, width = 5000, res = 350)
+#X11()
+ggplot(FullWhiteRef_df, aes(Wavelength, Radiance, color = Source), scales = "fixed")+
+  theme(panel.background = element_rect(fill = "white", colour = "grey50"), 
+        #legend.key.size = unit(0.5, "cm"),legend.text = element_text(size=25),
+        #legend.position = "none",
+        title = element_text(size=25),
+        strip.text = element_text(size = 25),
+        axis.text = element_text(size = 20),
+        axis.text.x = element_text(angle = 90)) +
+  geom_line(aes(Wavelength, Radiance), size = 2) +
+  facet_wrap(vars(Site_WhiteRef_Image))
+dev.off()
+
+jpeg("figures/Spectralon_vs_Tarp66pctReflWhiteRef_by_Site_Proportion.jpg", height = 3000, width = 5000, res = 350)
+#X11()
+ggplot(FullWhiteRef_df_wide, aes(Wavelength, Radiance_proportion, color = Site_WhiteRef_Image), scales = "fixed")+
+  theme(panel.background = element_rect(fill = "white", colour = "grey50"), 
+        #legend.key.size = unit(0.5, "cm"),legend.text = element_text(size=25),
+        #legend.position = "none",
+        title = element_text(size=25),
+        strip.text = element_text(size = 25),
+        axis.text = element_text(size = 20),
+        axis.text.x = element_text(angle = 90)) +
+  #  geom_line(aes(Wavelength, Radiance_diff), size = 2) #+
+    geom_smooth(aes(Wavelength, Radiance_proportion), size = 2) #+
+  #  geom_boxplot(aes(Wavelength, Radiance_proportion), size = 2) #+
+
+  #facet_wrap(vars(Site_WhiteRef_Image))
+dev.off()
 
 ##Plot tarp reflectance by site
-
 tarp_refl<-read.csv("./Data/Ground_Validation/WhiteRefs/Tarp/TarpSpectra.csv")
+head(tarp_refl)
 tarp_refl_r<-resample_df(tarp_refl)#, drop_existing = TRUE)
 ## Make histogram of values
 tarp_refl_for_hist<-tarp_refl_r %>% 
@@ -133,3 +236,4 @@ ggplot(tarp_refl_tall %>% dplyr::filter(site != "TwelveMile") %>% dplyr::filter(
  dev.off()
 
 table(tarp_refl_tall_)
+
