809cd121283888ad023368b1b79008d57439bd29
nelsopet <peter.ross.nelson@gmail.com>
2024-02-23 11:53:06 -0500

    spectralon pre flight, tarp median refl and single pixel used in radiocal compared by site

--- Scripts/TrainingDataCreation/Image/105_Compare_white_references_across_sites.R
+++ Scripts/TrainingDataCreation/Image/105_Compare_white_references_across_sites.R
@@ -7,20 +7,21 @@ allfiles<-list.files(path)
 imgs = grep(".hdr*", allfiles, value = TRUE)
 imgs<-imgs[1:4]
 #Print and paste list of names rather than messing with grepping out stuff
-imgs_names<-c("BisonGulch_100251_Bison_Gulch_line2_2019_08_12_01_07_28_sceneWhiteReference"       
-,"Bonanza_100066_2018_07_25_21_18_45_sceneWhiteReference"
-,"Chatanika_100130_ChatanikaFlight3_attempt2_2018_07_29_20_32_59_sceneWhiteReference"
-,"EightMile_100124_BlacktandardFlight2_2018_07_28_22_56_17_sceneWhiteReference")
+imgs_names_t<-c("BisonGulch_100251_Bison_Gulch_line2_2019_08_12_01_07_28_sceneWhiteReference")       
+#,"Bonanza_100066_2018_07_25_21_18_45_sceneWhiteReference"
+#,"Chatanika_100130_ChatanikaFlight3_attempt2_2018_07_29_20_32_59_sceneWhiteReference"
+#,"EightMile_100124_BlacktandardFlight2_2018_07_28_22_56_17_sceneWhiteReference")
+
 #,"MurphyDome_100158_MurphyDomeFlight1_2018_07_31_19_47_11_sceneWhiteReference"
 #,"TwelveMile_100241_12mile_line3_2019_08_09_21_28_52_sceneWhiteReference")
 
 #Read in the band names
 band_names <- read.csv("./assets/bands.csv")$x[1:326] %>% as.vector()
-
+band_path<-terra::rast(paste(path,imgs_names_t[1], sep="")
 #For each white reference, read in the image, cast it to dataframe, add site and band info
 # and rename columns
 WhiteRef_df<-lapply(1:length(imgs_names), function(x){ 
-band_path<-brick(paste(path,imgs_names[x], sep=""))
+band_path<-terra::rast(paste(path,imgs_names_t[x], sep=""))
 band_count<-names(band_path) %>% length()
 tst<-band_path
 df <- raster::rasterToPoints(tst) %>% 
@@ -43,6 +44,9 @@ WhiteRef_df %>% dplyr::filter(Site_WhiteRef_Image=="BisonGulch_100251_Bison_Gulc
 
 #Read in new white references taken from brightest section of calibration tarp (66%)
 #Flight lines for use in manuscript
+
+imgs_names<-c("Bison","Bonanza", "Eightmile","Chatanika")
+
 Bison_dir="M:/Alaska_DATA/Alaska_Summer2019/Data_by_site/Bison_Gulch/Imagery_60m/100251_Bison_Gulch_line2_2019_08_12_01_07_28/"
 Bonanza_dir="M:/Alaska_DATA/Alaska_Summer2018/Workspaces/Alaska/DatabyDate/72518/ImagingSpectrometer/DataFiles/100066_2018_07_25_21_18_45/"
 EightMile_dir = "M:/Alaska_DATA/Alaska_Summer2018/Workspaces/Alaska/DatabyDate/72818/ImagingSpectrometer/DataFolders/100124_BlacktandardFlight2_2018_07_28_22_56_17/"
@@ -71,17 +75,19 @@ df <- raster::rasterToPoints(tst) %>%
   t() %>%
   as.data.frame()
 colnames(df)<-"Radiance"
-df$Site_WhiteRef_Image<-imgs_names[x]
+df$Site<-imgs_names[x]
 df$Wavelength<-band_names
   return(df)
 })
 
 NewWhiteRef_df<-Reduce(function (x,y) merge(x, y, all=TRUE), NewWhiteRef_df)
 
+write.csv(NewWhiteRef_df, "Data/Ground_Validation/WhiteRefs/Tarp/SinglePixelWhiteRefs/Single_Tarp_pixel_white_refs.csv")
+head(NewWhiteRef_df)
 
 #jpeg("figures/WhiteRef_by_Site.jpg", height = 3000, width = 5000, res = 350)
 X11()
-ggplot(WhiteRef_df, aes(Wavelength, Radiance, color = Site_WhiteRef_Image), scales = "fixed")+
+ggplot(NewWhiteRef_df, aes(Wavelength, Radiance, color = Site_WhiteRef_Image), scales = "fixed")+
   theme(panel.background = element_rect(fill = "white", colour = "grey50"), 
         #legend.key.size = unit(0.5, "cm"),legend.text = element_text(size=25),
         #legend.position = "none",
