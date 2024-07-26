45aa112782694ea056bf2339e0126a3e1d2c94d0
nelsopet <peter.ross.nelson@gmail.com>
2024-07-25 21:30:01 -0400

    added writeSLI function from another outdated package and assessed accuracy of best model

---
+++ Scripts/ExploratoryDataAnalysis/summarize_accuracy_best_model.R
@@ -0,0 +1,27 @@
+source("./Functions/lecospectR.R")
+
+
+aggregation_key <- rjson::fromJSON(file = "./assets/pft_adj_list.json")
+
+
+#Explore a single model
+
+#Best adaboost model
+#Model path but only useful for looking at model. Specify model in config.json
+path = "./test/GridSearchResults/AdaBoost/862b797a-c8cb-485d-870c-e5f4742be5ce/862b797a-c8cb-485d-870c-e5f4742be5ce"
+mod<-load_model(paste(path,"model.rda",sep="/"))
+
+#Load testing dataset
+test_data<-read.csv("Data/v2/test_50nm_500.csv")
+test_data_fncGrp0<-change_aggregation(test_data$FncGrp1, 0, aggregation_key)
+colnames(test_data)
+test_data_pred<-predict(mod,test_data %>% subset(FncGrp1 != "Forb"))
+test_data_pred_fncGrp0<-change_aggregation(test_data_pred, 0, aggregation_key)
+
+accuracy(test_data_fncGrp0, test_data_pred_fncGrp0)
+conf_mat<-caret::confusionMatrix(data = as.factor(test_data_pred_fncGrp0), as.factor(test_data_fncGrp0))
+
+
+#Kappa stat is here
+conf_mat$overall
+conf_mat$table
